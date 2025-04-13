434e477f74c62c740e9955a15ff1bf9508883e6f
metsu971 <tet.drago.autochess@gmail.com>
2025-04-13 11:25:30 +0900

    improve layout

--- app/src/main/java/com/example/frontapp/ui/UserFormScreen.kt
+++ app/src/main/java/com/example/frontapp/ui/UserFormScreen.kt
@@ -1,14 +1,24 @@
 package com.example.frontapp.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.example.frontapp.model.User
-import com.example.frontapp.model.UserClient
 import com.example.frontapp.viewmodel.UserViewModel
-import kotlinx.coroutines.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,7 +43,8 @@ fun UserFormScreen(viewModel: UserViewModel) {
                 name = ""
                 isSubmitting = false
             },
-            enabled = !isSubmitting
+            enabled = !isSubmitting,
+            shape = RectangleShape
         ) {
             Text("登録する")
         }
--- app/src/main/java/com/example/frontapp/ui/UserListScreen.kt
+++ app/src/main/java/com/example/frontapp/ui/UserListScreen.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.frontapp.ui.theme.MaterialSoftGreen
+import com.example.frontapp.ui.theme.MaterialSoftRed
 import com.example.frontapp.viewmodel.UserViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,40 +31,50 @@ import kotlinx.coroutines.launch
 fun UserListScreen(viewModel: UserViewModel = remember { UserViewModel() }) {
     val users = viewModel.users.collectAsState()
 
-    Column {
+    Column(modifier = Modifier.fillMaxSize()) {
         UserFormScreen(viewModel = viewModel)
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(modifier = Modifier.padding(16.dp),
+        Button(
+            modifier = Modifier.padding(16.dp),
             onClick = {
-            viewModel.fetchUsers()
-        }) {
+                viewModel.fetchUsers()
+            }) {
             Text("ユーザ一覧を取得")
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn (
-            contentPadding = PaddingValues(16.dp)
-        ){
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
             items(users.value) { user ->
                 Column {
                     Text(text = "名前: ${user.name}")
                     Row {
-                        Button(onClick = {
-                            viewModel.updateUser(user.copy(name = "更新後-${user.name}"))
-                        }) {
+                        Button(
+                            colors = ButtonDefaults.textButtonColors(
+                                containerColor = MaterialSoftGreen,
+                                contentColor = Color.White
+                            ), onClick = {
+                                viewModel.updateUser(user.copy(name = "更新後-${user.name}"))
+                            }) {
                             Text("更新")
                         }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = {
-                            CoroutineScope(Dispatchers.IO).launch {
-                                user.id?.let {
-                                    viewModel.deleteUser(it)
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            colors = ButtonDefaults.textButtonColors(
+                                containerColor = MaterialSoftRed,
+                                contentColor = Color.White
+                            ), onClick = {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    user.id?.let {
+                                        viewModel.deleteUser(it)
+                                    }
                                 }
-                            }
-                        }) {
+                            }) {
                             Text("削除")
                         }
                     }
